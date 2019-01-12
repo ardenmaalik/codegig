8f62f32c3866ac0c2303dbd1f643c6d85be0b741
Arden Maalik <ardenmaalik@gmail.com>
2019-01-12 07:11:15 -0500

    test

--- config/database.js
+++ config/database.js
@@ -5,5 +5,8 @@ var db = {};
 if (config.use_env_variable) {
    module.exports = new Sequelize(process.env[config.use_env_variable]);
 } else {
-   module.exports = new Sequelize(config.database, config.username, config.password, config);
+   module.exports = new Sequelize(config.database, config.username, config.password, {
+    host: "ec2-54-225-121-235.compute-1.amazonaws.com", 
+    dialect: "postgres"
+   });
 }
