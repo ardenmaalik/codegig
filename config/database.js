21cda87934515ddd8efa666109fc3ca7a623700b
Arden Maalik <ardenmaalik@gmail.com>
2019-01-12 16:08:06 -0500

    test db

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || "development";
-var config = require('../node_modules/.bin/config/config.json')[env];
-var db = {};
-if (config.use_env_variable) {
-   module.exports = new Sequelize(process.env[config.use_env_variable]);
-} else {
-   module.exports = new Sequelize(config.database, config.username, config.password, {
-    host: "ec2-54-225-121-235.compute-1.amazonaws.com", 
-    dialect: "postgres"
-   });
-}
+module.exports = new Sequelize('codegig', 'postgres', 'hanan143', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
