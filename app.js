46c87b1f52f333de2e31fb36c7b2312ae652935f
Arden Maalik <ardenmaalik@gmail.com>
2019-01-12 00:31:34 -0500

    Add sequelize command and db client var

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  
+  client.connect();
 const express = require('express');//web framework
 const exphbs = require('express-handlebars');//display html and dynamic content
 const bodyParser = require('body-parser');//get data from form
