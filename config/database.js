19bc936f6b8da818c1b0870b03a8056d48e58b6e
Arden Maalik <ardenmaalik@gmail.com>
2019-01-12 06:47:55 -0500

    test db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || "production";
-var config = require(__dirname + '/../node_modules/.bin/config/config.json')[env];
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + '/config/config.json')[env];
 var db = {};
 if (config.use_env_variable) {
    module.exports = new Sequelize(process.env[config.use_env_variable]);
