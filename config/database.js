3050109489033816b626404c382bba7575098418
Arden Maalik <ardenmaalik@gmail.com>
2019-01-12 06:05:56 -0500

    test db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,20 @@
 const Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || 'production';
+var env = process.env.NODE_ENV || "production";
 var config = require(__dirname + '/../node_modules/.bin/config/config.json')[env];
 var db = {};
 if (config.use_env_variable) {
    module.exports = new Sequelize(process.env[config.use_env_variable]);
 } else {
-   module.exports = new Sequelize(config.database, config.username, config.password, config);
-};
+   module.exports = new Sequelize(config.database, config.username, config.password, config, {
+    host: 'https://salty-fortress-13433.herokuapp.com/',
+    dialect: 'postgres',
+    operatorsAliases: false,
+  
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  });
+}
